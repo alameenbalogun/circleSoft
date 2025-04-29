13dd763795f2e663539eb705cb32ea1c1ec631e8
Activedge <activedge@Activedge-MacBook-Pro-2.local>
2025-04-29 10:53:05 +0100

    improved container width:

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,10 +12,10 @@ export default function RootLayout({ children }) {
       <body className="relative bg-[#f8f8f8] w-full overflow-hidden">
         <Header />
         <div className="flex justify-normal items-start w-full h-dvh relative">
-          <div className="h-full w-[273px] relative">
+          <div className="h-full !max-w-[12%] !w-full !overflow-hidden">
             <SideBar />
           </div>
-          <div className="h-full w-full overflow-scroll pb-[41px]">
+          <div className="h-full max-w-[88%] w-full overflow-y-scroll overflow-hidden p-10 pb-24">
             {children}
           </div>
         </div>
