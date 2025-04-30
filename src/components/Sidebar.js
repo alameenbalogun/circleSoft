2337ebfa5ff0991bb50fe995bed143f6cbf5c678
Activedge <activedge@Activedge-MacBook-Pro-2.local>
2025-04-30 08:46:11 +0100

    use scale for responsive size on the pie_chart

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -12,8 +12,8 @@ import { CiLogout } from "react-icons/ci";
 export default function SideBar() {
   return (
     <div className="top-[66px] shadow-sm !h-[calc(100vh-66px)] bg-white flex flex-col overflow-y-auto">
-      <div className="flex gap-2 xl:gap-5 px-2 xl:!px-5 p-6">
-        <div className="flex gap-2 items-center">
+      <div className="flex gap-2 md:!gap-5 xl:!gap-10 px-2 md:!px-5 xl:!px-10 p-6">
+        <div className="flex gap-2 xl:!gap-3 items-center">
           <img src="/gavano.svg" alt="Gavano Image" width={40} />
           <div className="">
             <h1 className="font-bold text-sm xl:text-base">Gavano</h1>
@@ -23,47 +23,47 @@ export default function SideBar() {
         <img src="/menu.svg" alt="Menu " />
       </div>
       <div className="mt-3 flex flex-col h-full">
-        <div>  
+        <div>
           {" "}
           <ul className="mb-4 cursor-pointer">
             <Link
               href="/"
-              className="flex items-center gap-4 p-2 hover:bg-[#F8F8F8] hover:text-[#5932EA] h-[50px] !px-5"
+              className="flex items-center gap-4 p-2 hover:text-[#FFF] h-[50px] hover:bg-[#5932EA] px-5 xl:!px-10"
             >
               <LuLayoutDashboard className="w-6 h-6 object-cover" />
               <p className="text-[16px]">Dashboard</p>
             </Link>
             <Link
               href="/finance"
-              className="flex items-center gap-4 p-2 group !px-5 hover:bg-[#F8F8F8] h-[50px] hover:text-[#5932EA]"
+              className="flex items-center gap-4 p-2 group px-5 xl:!px-10 hover:text-[#FFF] h-[50px] hover:bg-[#5932EA]"
             >
               <MdInsertChartOutlined className="w-6 h-6 object-cover" />
               <p className="text-[16px] ">Finance</p>
             </Link>
             <Link
               href="/employee"
-              className="flex items-center gap-4 p-2  !px-5  hover:bg-[#F8F8F8] h-[50px] hover:text-[#5932EA]"
+              className="flex items-center gap-4 p-2 px-5 xl:!px-10 hover:text-[#FFF] h-[50px] hover:bg-[#5932EA]"
             >
               <LiaIdCard className="w-6 h-6 object-cover" />
               <p className="text-[16px]">Employees</p>
             </Link>
             <Link
               href="/company"
-              className="flex items-center gap-4 p-2  !px-5 hover:bg-[#F8F8F8] h-[50px] hover:text-[#5932EA]"
+              className="flex items-center gap-4 p-2 px-5 xl:!px-10 hover:text-[#FFF] h-[50px] hover:bg-[#5932EA]"
             >
               <TbBriefcase className="w-6 h-6 object-cover" />
               <p className="text-[16px]">Company</p>
             </Link>
             <Link
               href="/candidate"
-              className="flex items-center gap-4 p-2  !px-5  hover:bg-[#F8F8F8] h-[50px] hover:text-[#5932EA]"
+              className="flex items-center gap-4 p-2 px-5 xl:!px-10 hover:text-[#FFF] h-[50px] hover:bg-[#5932EA]"
             >
               <IoIosContact className="w-6 h-6 object-cover" />
               <p className="text-[16px]">Candidate</p>
             </Link>
             <Link
               href="/calendar"
-              className="flex items-center gap-4 p-2 !px-5  hover:bg-[#F8F8F8] h-[50px] hover:text-[#5932EA]"
+              className="flex items-center gap-4 p-2 px-5 xl:!px-10 hover:text-[#FFF] h-[50px] hover:bg-[#5932EA]"
             >
               <LuCalendarDays className="w-6 h-6 object-cover" />
               <p className="text-[16px]">Calendar</p>
@@ -77,14 +77,14 @@ export default function SideBar() {
           <ul className="mt-3 cursor-pointer">
             <Link
               href="/profile"
-              className="flex items-center gap-4 p-2  !px-5  hover:text-[#FFF] h-[50px] hover:bg-[#5932EA]"
+              className="flex items-center gap-4 p-2 px-5 xl:!px-10 hover:text-[#FFF] h-[50px] hover:bg-[#5932EA]"
             >
               <BsPerson className="w-6 h-6 object-cover" />
               <p className="text-[16px]">Profile</p>
             </Link>
             <Link
               href="/settings"
-              className="flex items-center gap-4 p-2  !px-5  hover:bg-[#F8F8F8] h-[50px] hover:text-[#5932EA]"
+              className="flex items-center gap-4 p-2 px-5 xl:!px-10 hover:text-[#FFF] h-[50px] hover:bg-[#5932EA]"
             >
               <IoSettingsOutline className="w-6 h-6 object-cover" />
               <p className="text-[16px]">Settings</p>
@@ -94,7 +94,7 @@ export default function SideBar() {
 
         <Link
           href="/logout"
-          className="flex items-center gap-4 p-2 !px-5 cursor-pointer bg-red-500 text-white hover:bg-transparent h-[50px] hover:text-red-500 mt-auto"
+          className="flex items-center gap-4 p-2 px-5 xl:!px-10 cursor-pointer bg-red-500 text-white hover:bg-transparent h-[50px] hover:text-red-500 mt-auto"
         >
           <CiLogout className="w-6 h-6 object-cover" />
           <p className="text-[16px]">Logout</p>
