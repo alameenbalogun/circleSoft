e5b3012ea255ef49dbdc414902c255acac6b65ff
Nene_dev <nafisafaruna@gmail.com>
2025-05-15 12:42:58 +0100

    fixed routes

--- src/components/routes.js
+++ src/components/routes.js
@@ -8,17 +8,17 @@ import { FaCalendarAlt, FaClipboardList, FaCog, FaIdBadge, FaSignOutAlt, FaUserA
 export const sidebarRoutes = [
   {
     title: "Dashboard",
-    path: "/",
+    path: "/dashboard",
     icon: <MdDashboard className="w-6 h-6 object-cover" />,
   },
   {
     title: "Finance",
-    path: "/finance",
+    path: "/dashboard/finance",
     icon: <BiSolidReport className="w-6 h-6 object-cover" />,
   },
   {
     title: "Calendar",
-    path: "/calendar",
+    path: "/dashboard/calendar",
     icon: <FaCalendarAlt className="w-6 h-6 object-cover" />,
   },
   {
@@ -26,29 +26,29 @@ export const sidebarRoutes = [
     children: [
       {
         title: "Manage Employees",
-        path: "/employee",
+        path: "/dashboard/employee",
         icon: <FaIdBadge className="w-6 h-6 object-cover" />,
       },
       {
         title: "Directory",
-        path: "/company",
+        path: "/dashboard/company",
         icon: <TbBriefcase className="w-6 h-6 object-cover" />,
       },
       {
         title: "Candidate",
-        path: "/candidate",
+        path: "/dashboard/candidate",
         icon: <IoIosContact className="w-6 h-6 object-cover" />,
       },
     ],
   },
   {
     title: "Profile",
-    path: "/profile",
+    path: "/dashboard/profile",
     icon: <FaUserAlt className="w-6 h-6 object-cover" />,
   },
   {
     title: "Report",
-    path: "/report",
+    path: "/dashboard/report",
     icon: <FaClipboardList className="w-6 h-6 object-cover" />,
   },
   {
@@ -56,17 +56,17 @@ export const sidebarRoutes = [
     children: [
       {
         title: "Jobs",
-        path: "/employee",
+        path: "/dashboard/employee",
         icon: <IoIosBriefcase className="w-6 h-6 object-cover" />,
       },
       {
         title: "Candidates",
-        path: "/company",
+        path: "/dashboard/company",
         icon: <BsBriefcaseFill className="w-6 h-6 object-cover" />,
       },
       {
         title: "Settings",
-        path: "/candidate",
+        path: "/dashboard/candidate",
         icon: <IoIosContact className="w-6 h-6 object-cover" />,
       },
     ],
